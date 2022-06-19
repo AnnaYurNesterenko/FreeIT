7b1f6f4a0cf00e3e09d5ebe62027ee8709c9c723
AnnaYurNesterenko <philipuk.anna@gmail.com>
2022-06-19 18:35:09 +0300

    lession2: numeric type int

---
+++ Lession2/src/com/Nesterenko/lession2/HomeworkTask2.java
@@ -0,0 +1,13 @@
+package com.Nesterenko.lession2;
+
+public class HomeworkTask2 {
+    public static void main(String[] args) {
+        int myInt = 234;
+        System.out.println(myInt);
+
+
+
+    }
+
+
+}
