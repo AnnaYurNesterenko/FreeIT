4883fcf3daef6100d169d960530223f90f730c57
AnnaYurNesterenko <philipuk.anna@gmail.com>
2022-06-19 19:43:56 +0300

    lession4: Homework - comparing two numbers

---
+++ Lession4/src/com/Nesterenko/lession4/Homework.java
@@ -0,0 +1,15 @@
+package com.Nesterenko.lession4;
+
+public class Homework {
+    public static void main(String[] args) {
+        int a = 596;
+        int b = 485;
+        System.out.println(a != b );
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+}
